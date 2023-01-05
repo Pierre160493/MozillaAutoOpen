37978ad223b2cea91003d46d44893ad31e8b6366
Pierre160493 <pierre160493@gmail.com>
2023-01-05 12:08:08 +0100

    Fonctionne

---
+++ AutoOpen.bat
@@ -0,0 +1,11 @@
+cd "C:\Program Files\Firefox Developer Edition"
+REM Partie Inde
+start firefox.exe "https://www.supremacy1914.com/game.php?L=11&bust=1#/game/:gameID=6578012"
+REM Partie Kazak
+start firefox.exe "https://www.supremacy1914.com/game.php?L=11&bust=1#/game/:gameID=6555670"
+REM Partie Equat
+start firefox.exe "https://www.supremacy1914.com/game.php?L=11&bust=1#/game/:gameID=6553741"
+REM Partie Lyb
+start firefox.exe "https://www.supremacy1914.com/game.php?L=11&bust=1#/game/:gameID=6365951"
+REM Partie Inde_Old
+start firefox.exe "https://www.supremacy1914.com/game.php?L=11&bust=1#/game/:gameID=6344884"
